5e94990d9559bb18754bfbdb5175db9f7a681950
fbbDark <Fabian_Boss_@Outlook.com>
2015-09-28 03:40:27 -0500

    task compileJade

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,9 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass');
+
+gulp.task('compileJade', function() {
+  gulp.src('src/index.jade')
+  .pipe(jade({pretty: true}))
+  .pipe(gulp.dest('dist/'))
+});
